12ed1e6100ec08ab7c07cffa29b6db13aacd80a0
Vadim Leontiev <vleontiev@englishdom.com>
2016-02-03 11:36:13 +0000

    Changed folder structure. Added script for server run to composer

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-/* php -S localhost:8080 -t web web/index.php */
-
 $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
