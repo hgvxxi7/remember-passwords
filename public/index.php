551c63b0d601e22d41de77d6d80e70bb81251d1e
newage <vadim.leontiev@gmail.com>
2016-02-03 22:48:25 +0200

    Added localisation

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+/* Change directory to project home */
+chdir(dirname(__DIR__));
+require_once 'vendor/autoload.php';
 
 $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
@@ -13,7 +15,15 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 $app->get("/", "PasswordManager\\Controller\\IndexController::indexAction");
-$app->get("/registration", "PasswordManager\\Controller\\UserController::registrationAction");
+$app->get("/{_locale}/registration", "PasswordManager\\Controller\\UserController::registrationAction");
+$app->get("/registration", "PasswordManager\\Controller\\UserController::registrationAction")
+    ->value('_locale', 'ru');
 
+$app->register(new \Silex\Provider\TranslationServiceProvider());
+$app->extend('translator', function (\Silex\Translator $translator, $app) {
+    $translator->addLoader('array', new \Symfony\Component\Translation\Loader\PhpFileLoader());
+    $translator->addResource('array', 'data/translation/ru.php', 'ru');
+    return $translator;
+});
 $app['debug'] = true;
 $app->run();
